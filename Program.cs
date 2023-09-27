940c9e7d7ecd8619f11ef61940f56437263b95dd
Karim Altir <mhd.altir@student.ehb.be>
2023-09-27 11:29:06 +0200

    making a main to run the program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Diversen;
+
+Student student = new Student { Voornaam = "Joe", FamilieNaam = "Biden", ID = 1, Inschrijving = DateTime.Now };
+
+string voornaam, familienaam;
+
+Tuple<string, string> NaamVanStudent = Student.Name(1);
+
+voornaam = NaamVanStudent.Item1;
+familienaam = NaamVanStudent.Item2;
+
+StudentNaam sn = Student.StructNaam(1);
+voornaam = sn.Voornaam;
+familienaam = sn.FamilieNaam;
